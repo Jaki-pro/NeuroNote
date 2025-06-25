ef5735ca706756a23f1b9314e8fc3af8f203303e
Jakaria Hossain <mdjak8980@gmail.com>
2025-06-25 12:29:12 +0600

    add mui custom theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,46 @@
-import { Box, Typography } from '@mui/material';
+import { Box, CssBaseline, ThemeProvider, Typography } from '@mui/material';
 import { Route, Routes } from 'react-router';
 import { NotFound, Dashboard } from './pages/';
 import AppLaout from './components/layout/AppLaout';
+import { theme } from './ui/theme';
 function App() {
 
   return (
-    <Routes>
-      <Route path='/' element={
-        <AppLaout/>
-      }>
-        <Route index path='/' element={
-          <Dashboard />
-        } />
-        <Route path='/bookmarks' element={
-          <Box>
-            <Typography variant="h4">
-              Bookmarks
-            </Typography>
-          </Box>
-        } />
-        <Route path='/settings' element={
-          <Box>
-            <Typography variant="h4">
-              Settings
-            </Typography>
-          </Box>
-        } />
-        <Route path='/archive' element={
-          <Box>
-            <Typography variant="h4" >
-              Archive
-            </Typography>
-          </Box>
-        } />
-        <Route path='*' element={<NotFound />} />
-      </Route>
-    </Routes>
+
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route path='/' element={
+          <AppLaout />
+        }>
+          <Route index path='/' element={
+            <Dashboard />
+          } />
+          <Route path='/bookmarks' element={
+            <Box>
+              <Typography variant="h4">
+                Bookmarks
+              </Typography>
+            </Box>
+          } />
+          <Route path='/settings' element={
+            <Box>
+              <Typography variant="h4">
+                Settings
+              </Typography>
+            </Box>
+          } />
+          <Route path='/archive' element={
+            <Box>
+              <Typography variant="h4" >
+                Archive
+              </Typography>
+            </Box>
+          } />
+          <Route path='*' element={<NotFound />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
   )
 }
 
